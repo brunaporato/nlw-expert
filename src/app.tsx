40561caed847d5ee8b0ce57ba5caaaf4672b6467
Bruna <brunaporato@gmail.com>
2024-03-13 11:59:15 -0300

    feat: layout done

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,30 @@
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+const note = {
+  date: new Date(),
+  content: 'Hello world',
+}
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">hello world</h1>
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NlW Expert" />
+      <form action="" className="w-full">
+        <input
+          type="text"
+          placeholder="Search your notes..."
+          className="w-full bg-transparent outline-none text-3xl font-semibold tracking-tight placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard />
+        <NoteCard note={note} />
+      </div>
+    </div>
+  )
 }
