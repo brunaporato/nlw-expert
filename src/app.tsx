05cbe1ebede5f6471748dd7aacf23bad5e7ec11a
Bruna <brunaporato@gmail.com>
2024-03-14 15:31:09 -0300

    layout: fix responsiveness

--- src/app.tsx
+++ src/app.tsx
@@ -46,7 +46,7 @@ export function App() {
       : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NlW Expert" />
       <form action="" className="w-full">
         <input
@@ -60,7 +60,7 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard onNoteSaved={onNoteSaved} />
         {filteredNotes &&
           filteredNotes.map((note) => {
